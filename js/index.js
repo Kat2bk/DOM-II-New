ff5b4f2ed6eedb28cbc29d10943cc4c7ad11a1a6
unknown <Kat2bk@gmail.com>
2020-02-14 17:45:51 -0800

    added a few eventListeners that effects images and navigation anchor tags, but there is a few bugs to iron out

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
 // Your code goes here
+const images = document.getElementsByTagName("img");
+// console.log(images);
+
+Array.from(images).forEach(item => {
+  item.addEventListener("mouseenter", e => {
+    item.style.transform = "scale(1.2)";
+    item.style.transition = "all 0.3s";
+  });
+});
+
+Array.from(images).forEach(function(item) {
+  item.addEventListener("mouseleave", function(e) {
+    item.style.transform = "scale(1)";
+  });
+});
+
+// why does image scale over other elements?
+
+const anchors = document.querySelectorAll("header nav a");
+
+anchors.forEach(function(item) {
+  item.addEventListener("click", event => {
+    item.style.transform = "scale(1.5)";
+    if (item.style.transform === "scale(1.5)") {
+      event.target.removeEventListener("click");
+    }
+  });
+});
+
+// how to add another click that removes transform
